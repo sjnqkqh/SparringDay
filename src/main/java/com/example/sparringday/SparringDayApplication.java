8e9d0e3c8686267f4d9f2c5d78e791552b65e62b
junlyul <jlyang@fym.im>
2024-01-17 21:38:33 +0900

    Project Set-up

---
+++ src/main/java/com/example/sparringday/SparringDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.sparringday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparringDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparringDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sparringday/SparringDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparringday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparringDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
