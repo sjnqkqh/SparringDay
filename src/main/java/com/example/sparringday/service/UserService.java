9068f22a0d7ec9e38f94f1faf1a9897383553f58
real-dev <sjn3649@naver.com>
2024-01-31 21:40:42 +0900

    복싱 관련 코드 이넘 및 추가 정보 DTO 생성

--- src/main/java/com/example/sparringday/service/UserService.java
+++ src/main/java/com/example/sparringday/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.sparringday.config.CommonException;
+import com.example.sparringday.dto.user.request.UserBoxingInfoReqDto;
 import com.example.sparringday.dto.user.request.CreateUserReqDto;
 import com.example.sparringday.entity.User;
 import com.example.sparringday.repository.UserRepository;
@@ -35,21 +36,6 @@ public class UserService {
 	}
 
 	@Transactional(readOnly = true)
-	public User findUserById(Long userId) {
-		return userRepository.findById(userId).orElseThrow(() -> {
-			log.error("[UserService.findUserById] No user exist. userId:" + userId);
-			return new CommonException(ApiExceptionCode.USER_NOT_EXIST_ERROR);
-		});
-	}
-
-	@Transactional(readOnly = true)
-	protected void checkUserLoginIdDuplication(String loginId) {
-		if (userRepository.existsByLoginId(loginId)) {
-			throw new CommonException(ApiExceptionCode.DUPLICATE_LOGIN_ID_EXIST_ERROR);
-		}
-	}
-
-	@Transactional(readOnly = true)
 	public User login(String loginId, String password) {
 		User user = userRepository.findUserByLoginId(loginId)
 			.orElseThrow(() -> new CommonException(ApiExceptionCode.USER_NOT_EXIST_ERROR));
@@ -57,6 +43,25 @@ public class UserService {
 		checkPasswordMatch(password, user.getEncryptedPassword());
 
 		return user;
+	}
+
+	@Transactional
+	public void updateUserBoxingInfo(User user, UserBoxingInfoReqDto reqDto){
+
+	}
+
+	protected void checkUserLoginIdDuplication(String loginId) {
+		if (userRepository.existsByLoginId(loginId)) {
+			throw new CommonException(ApiExceptionCode.DUPLICATE_LOGIN_ID_EXIST_ERROR);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public User findUserById(Long userId) {
+		return userRepository.findById(userId).orElseThrow(() -> {
+			log.error("[UserService.findUserById] No user exist. userId:" + userId);
+			return new CommonException(ApiExceptionCode.USER_NOT_EXIST_ERROR);
+		});
 	}
 
 	private void checkPasswordMatch(String receivedPassword, String storedPassword) {
